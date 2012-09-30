41b150e393a0ba78e02dbaa18e2ef69fe63f9eff
Liaits <ryua775@gmail.com>
2012-09-30 11:42:11 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThusastSrt::Application.config.secret_token = '2eca4f4bda2257442f2d75eb43ba35ac3b18067b1649a9bc8d46b313e6fba2d0ef2d2dd978c12a4cd0101e9627426a90d21641f3a3524c324c99724de1a09027'
