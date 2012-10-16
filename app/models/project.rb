7a5c5f3e2ed52c81fbdd7c73e7e76f253adb804c
Liaits <ryua775@gmail.com>
2012-10-16 18:29:50 +0800

    generated devise views

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
   attr_accessible :author1, :author2, :author3, :author4, :department, :name, :prize, :professor, :status, :user_id
   has_one :user
+  has_many :authors
 end
